f5e53874d586dd0a6b00d2308a6653526b8ad8d4
Lenny Amstutz <lennyamstutz@gmail.com>
2026-08-26 13:53:03 +0200

    main py

--- Lenny/Mission/mission_1.py
+++ Lenny/Mission/mission_1.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+import sys
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 from Actions.communication_commands import buy, sell
 from Actions.steering_commands import set_target, wait_until_in_reach
@@ -16,11 +19,10 @@ def sell_at_core(amount):
     return sell(CORE, ITEM, amount)
 
 
-amounts = [4, 8, 12]
-for i, amount in enumerate(amounts):
-    buy_at_azura(amount)
-    if i < len(amounts) - 1:
-        sell_at_core(amount)
-
-set_target({"x": 7000, "y": 7000})
-
+def run():
+    amounts = [4, 8, 12]
+    for i, amount in enumerate(amounts):
+        buy_at_azura(amount)
+        if i < len(amounts) - 1:
+            sell_at_core(amount)
+    set_target({"x": 7000, "y": 7000})
