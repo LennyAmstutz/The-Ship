fbbee697f20f9f6849e60700927690c6638e9605
Lenny Amstutz <lennyamstutz@gmail.com>
2026-08-26 13:33:23 +0200

    main py

---
+++ Lenny/main.py
@@ -0,0 +1,7 @@
+# --- Hier umschalten: nur EINE Zeile aktiv lassen ----------------------
+from Mission.mission_1 import run
+# from Mission.mission_2 import run
+# from Mission.mission_3 import run
+# -----------------------------------------------------------------------
+
+run()
