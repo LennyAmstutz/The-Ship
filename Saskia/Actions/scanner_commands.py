40d3f039746b706fe1f1edfbf36aa60178acf99b
Lenny Amstutz <lennyamstutz@gmail.com>
2026-09-02 14:17:41 +0200

    print add

--- Saskia/Actions/scanner_commands.py
+++ Saskia/Actions/scanner_commands.py
@@ -1,18 +1,12 @@
 import json
 import pika
-from config import consume_host, consume_port, RABBITMQ_USER, RABBITMQ_PASS
+from Saskia.config import consume_host, consume_port
 
 EXCHANGE = "scanner/detected_objects"
 
 def detected_objects():
-    credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host=consume_host,
-            port=consume_port,
-            credentials=credentials,
-            socket_timeout=5,
-        )
+        pika.ConnectionParameters(host=consume_host, port=consume_port, socket_timeout=5)
     )
     channel = connection.channel()
     channel.exchange_declare(exchange=EXCHANGE, exchange_type="fanout")
